619ac4dec5bd3ad68c2c89da8077d7afa48a7b81
MateuszRaciborski <45817017+MateuszRaciborski@users.noreply.github.com>
2026-02-08 08:16:32 +0100

    Create constants.js

---
+++ js/constants.js
@@ -0,0 +1,11 @@
+export const GRID_W = 34;
+export const GRID_H = 24;
+export const CELL   = 30;
+
+export const BULLET_SPEED_PX_PER_MS = 0.5;
+
+export const fortInterior = { x:[10,23], y:[6,14] };
+export const ATT_START_SECTORS = ['99','98','95','101','102','105','120'];
+
+export const FLAG_SECTOR_ID = '63';
+export const FLAG_CELLS = [(16,9),(16,10),(17,9),(17,10)]; // info orientacyjnie
